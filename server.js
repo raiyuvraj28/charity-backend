5c84492f9e3d06e63d33b7e58f5855a0b941723a
Yuvraj Rai <raiyuvraj28@gmail.com>
2026-05-15 17:49:53 +0530

    change index file

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 
 app.use('/api', apiRoutes);
-
+app.get("/",(req,res)=>{
+  res.send("server is running successfully")
+})
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 10000 })
