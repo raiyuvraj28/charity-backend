a71de778af486a22b70419817037c3743be1450a
yuvraj <raiyuvraj28@gmail.com>
2026-05-16 10:54:42 +0530

    change files

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 
+app.get('/', (req, res) => {
+  res.json({ status: 'ok', service: 'charity-backend' });
+});
+
 app.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 5000;
